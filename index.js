8181b781027925795fd69dd9fca572f29673fc11
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-10 13:41:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // write your code below!
 
 var name = "Christopher"
-var height = "80"
+var height = 80
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
+
 
 
